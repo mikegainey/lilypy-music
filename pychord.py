f1f3cfaf97e8cfc65cf16fe1cf04724ab8971493
Michael Gainey <mikeg9b@gmail.com>
2018-03-19 21:43:48 -0500

    ran whitespace-cleanup

--- pychord.py
+++ pychord.py
@@ -1,17 +1,17 @@
 letters = 'a', 'b', 'c', 'd', 'e', 'f', 'g'
-notes = (['a', 'gss', 'bff'], ['as', 'bf'], ['b', 'cf', 'ass'], ['bs', 'c', 'dff'], ['cs', 'df'], ['d', 'css', 'eff'], 
+notes = (['a', 'gss', 'bff'], ['as', 'bf'], ['b', 'cf', 'ass'], ['bs', 'c', 'dff'], ['cs', 'df'], ['d', 'css', 'eff'],
          ['ds', 'ef'], ['e', 'ff', 'dss'], ['es', 'f', 'gff'], ['fs', 'gf'], ['g', 'fss', 'aff'], ['gs', 'af'])
-chords = {  'maj' : [[1, 0], [3, 4], [5, 7]], 'min' : [[1, 0], [3, 3], [5, 7]], 
+chords = {  'maj' : [[1, 0], [3, 4], [5, 7]], 'min' : [[1, 0], [3, 3], [5, 7]],
             'dim' : [[1, 0], [3, 3], [5, 6]], 'aug' : [[1, 0], [3, 4], [5, 8]],
-            'mM7', 
+            'mM7',
           }
 
 
 
 def chord(one, quality):
-    
+
     chord = []
-    
+
     # find the note letter and the index in the notes list
     for note in notes:
         if one in note:
@@ -23,19 +23,18 @@ def chord(one, quality):
 
     # make a list with degrees (1, 3, 5) and halfstep intervals (0, 4, 7)
     degrees = [degree for degree in chords[quality]]
-    
+
     for note in range(len(degrees)):
-        
+
         # compute the letter names of the chord tones
         letter_x = (one_letter_x + degrees[note][0] - 1) % 7
         chord_letter = letters[letter_x]
-        
+
         halfstep_interval = (one_index + degrees[note][1]) % 12
         pitch_class = notes[halfstep_interval]
-                
+
         for pc in range(len(pitch_class)):				# check each note of the pitch class
             if chord_letter == pitch_class[pc][0]:		# do the note letters match?
                 chord.append(pitch_class[pc])			# append the note to the output list
-    
-    return chord
 
+    return chord
--- pymode.py
+++ pymode.py
@@ -1,5 +1,5 @@
 letters = 'a', 'b', 'c', 'd', 'e', 'f', 'g'
-notes = (['a', 'gss', 'bff'], ['as', 'bf'], ['b', 'cf', 'ass'], ['bs', 'c', 'dff'], ['cs', 'df'], ['d', 'css', 'eff'], 
+notes = (['a', 'gss', 'bff'], ['as', 'bf'], ['b', 'cf', 'ass'], ['bs', 'c', 'dff'], ['cs', 'df'], ['d', 'css', 'eff'],
          ['ds', 'ef'], ['e', 'ff', 'dss'], ['es', 'f', 'gff'], ['fs', 'gf'], ['g', 'fss', 'aff'], ['gs', 'af'])
 major_scale = 2, 2, 1, 2, 2, 2, 1
 
@@ -10,7 +10,7 @@ def mode(one, mode):
     for mode_x in range(7):
         step = major_scale[(mode_x + mode - 1) % 7]
         pattern.append(step)
-    
+
     # is the argument (one) a valid note?
     for note in notes:
        if one in note:
@@ -31,14 +31,8 @@ def mode(one, mode):
        pitch_class = notes[scale_note_x]                # the pitch class of the scale degree
        scale_letter_x = (one_letter_x + degree) % 7     # index of the letter of the scale degree in letters
        scale_letter = letters[scale_letter_x]           # the letter of the scale degree in letters
-       
+
        for pc in range(len(pitch_class)):               # check each note of the pitch class
          if scale_letter == pitch_class[pc][0]:         # do the note letters match?
           scale.append(pitch_class[pc])                 # append the note to the output list
     return scale
-
-
-
-
-    
-    
