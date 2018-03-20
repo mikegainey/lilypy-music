4009c1fabe8fdcc893f5304bc796fc34e1855ff2
Michael Gainey <mikeg9b@gmail.com>
2018-03-20 00:28:47 -0500

    added docstring, converted print to python3, used enumerate

--- pychord.py
+++ pychord.py
@@ -1,25 +1,27 @@
-letters = 'a', 'b', 'c', 'd', 'e', 'f', 'g'
+# define constants
+letters = ('a', 'b', 'c', 'd', 'e', 'f', 'g')
 notes = (['a', 'gss', 'bff'], ['as', 'bf'], ['b', 'cf', 'ass'], ['bs', 'c', 'dff'], ['cs', 'df'], ['d', 'css', 'eff'],
          ['ds', 'ef'], ['e', 'ff', 'dss'], ['es', 'f', 'gff'], ['fs', 'gf'], ['g', 'fss', 'aff'], ['gs', 'af'])
-chords = {  'maj' : [[1, 0], [3, 4], [5, 7]], 'min' : [[1, 0], [3, 3], [5, 7]],
-            'dim' : [[1, 0], [3, 3], [5, 6]], 'aug' : [[1, 0], [3, 4], [5, 8]],
-            'mM7',
-          }
-
+chords = {'maj' : [[1, 0], [3, 4], [5, 7]], 'min' : [[1, 0], [3, 3], [5, 7]],
+          'dim' : [[1, 0], [3, 3], [5, 6]], 'aug' : [[1, 0], [3, 4], [5, 8]]}
 
 
 def chord(one, quality):
+    '''chord(one : str, quality : str) -> [str]
+    Given a starting note (of the format found in the notes constant) and a quality
+    (a key in the chords dictionary), return a list representing the notes of the chord.'''
 
     chord = []
 
     # find the note letter and the index in the notes list
-    for note in notes:
+    for index, note in enumerate(notes):
         if one in note:
-            one_note = one                                                              # the note
-            one_letter = one[0]                                                 # just the letter part
-            one_letter_x = letters.index(one_letter)    # the index of the letter in letters
-            one_index = notes.index(note)                               # the index of the note in notes
+            one_note = one                           # the note
+            one_letter = one[0]                      # just the letter part
+            one_letter_x = letters.index(one_letter) # the index of the letter in letters
+            one_index = index
             break
+        # what if the starting note is not found in notes?
 
     # make a list with degrees (1, 3, 5) and halfstep intervals (0, 4, 7)
     degrees = [degree for degree in chords[quality]]
@@ -33,8 +35,8 @@ def chord(one, quality):
         halfstep_interval = (one_index + degrees[note][1]) % 12
         pitch_class = notes[halfstep_interval]
 
-        for pc in range(len(pitch_class)):                              # check each note of the pitch class
-            if chord_letter == pitch_class[pc][0]:              # do the note letters match?
-                chord.append(pitch_class[pc])                   # append the note to the output list
+        for pc in range(len(pitch_class)):         # check each note of the pitch class
+            if chord_letter == pitch_class[pc][0]: # do the note letters match?
+                chord.append(pitch_class[pc])      # append the note to the output list
 
     return chord
