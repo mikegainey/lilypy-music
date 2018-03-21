95906f06543f357270451bfcd8ef8c6d0702601e
Michael Gainey <mikeg9b@gmail.com>
2018-03-20 21:44:26 -0500

    it works, but it needs tidying

--- scratch/wholeUp.py
+++ scratch/wholeUp.py
@@ -1,17 +1,27 @@
 letters = 'a', 'b', 'c', 'd', 'e', 'f', 'g'
-notes = (['a', 'gss', 'bff'], ['as', 'bf'], ['b', 'cf', 'ass'], ['bs', 'c', 'dff'], ['cs', 'df'], ['d', 'css', 'eff'],
-         ['ds', 'ef'], ['e', 'ff', 'dss'], ['es', 'f', 'gff'], ['fs', 'gf'], ['g', 'fss', 'aff'], ['gs', 'af'])
 major_scale = 2, 2, 1, 2, 2, 2, 1
 steps = {'c':2, 'd':2, 'e':1, 'f':2, 'g':2, 'a':2, 'b':1}
-modifiers = {'ff':-2, 'f':-1, 's':1, 'ss':2}
+modifiers = {'ff':2, 'f':1, 'n':0, 's':-1, 'ss':-2} # what a modifier on the lower note does
+umod = {0:'ss', 1:'s', 2:'', 3:'f', 4:'ff'}
 
-# an alternate algorithm to find the note a whole-step up from the given note
+# it works, but it needs tidying
 
 def wholeUp(one):
+    '''wholeUp(one : str) -> str
+    Given a starting note, return the note up one whole step.
+    '''
     one_letter = one[0] # letter of the input note
-    one_modifier = one[1:] # modifier of the input note
+    one_letter_x = letters.index(one_letter)
+    next_x = (one_letter_x + 1) % 7
+    upper_letter = letters[next_x]
+
+    if len(one) > 1:
+        one_modifier = one[1:] # modifier of the input note
+    else:
+        one_modifier = 'n'
+
     half_steps = steps[one_letter] # starting number of halfsteps from one_letter to the next letter
     half_steps += modifiers[one_modifier]
-    print(f"letter = {one_letter}, mod = {one_modifier}, half_steps to next letter = {half_steps}")
-
+    upper_note = upper_letter + umod[half_steps]
+    return upper_note
 
