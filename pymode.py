65f9fa8deee81ffbdf308037d589c47885d213fc
Michael Gainey <mikeg9b@gmail.com>
2018-03-21 05:22:05 -0500

    improved the mode pattern algorithm (no loop!)

--- pymode.py
+++ pymode.py
@@ -1,8 +1,8 @@
 # define constants
-letters = 'a', 'b', 'c', 'd', 'e', 'f', 'g'
+letters = ('a', 'b', 'c', 'd', 'e', 'f', 'g')
 notes = (['a', 'gss', 'bff'], ['as', 'bf'], ['b', 'cf', 'ass'], ['bs', 'c', 'dff'], ['cs', 'df'], ['d', 'css', 'eff'],
          ['ds', 'ef'], ['e', 'ff', 'dss'], ['es', 'f', 'gff'], ['fs', 'gf'], ['g', 'fss', 'aff'], ['gs', 'af'])
-major_scale = 2, 2, 1, 2, 2, 2, 1
+major_scale = (2, 2, 1, 2, 2, 2, 1)
 
 
 def mode(one, mode):
@@ -11,33 +11,30 @@ def mode(one, mode):
     (where 1 = Ionian, 2 = Dorian, etc.), return a list representing the scale
     starting on the given starting note.'''
 
-    pattern = []
-    for mode_x in range(7):
-        step = major_scale[(mode_x + mode - 1) % 7]
-        pattern.append(step)
+    split = mode - 1
+    pattern = major_scale[split:] + major_scale[:split]
 
     # is the argument (one) a valid note?
     for index, note in enumerate(notes):
         if one in note:
             one_note = one                             # the note
+            one_index = index                          # the index of the note in notes
             one_letter = one[0]                        # just the letter part
             one_letter_x = letters.index(one_letter)   # the index of the letter in letters
-            one_index = index
-            # one_index = notes.index(note)              # the index of the note in notes
             break
     else:
-        print(f"The note entered is not in the notes constant:  {one}\n")
-        return
+        print(f"Error: The input note was not recognized:  '{one}'\n")
+        return None
 
     scale = []
     for degree in range(8):
-        halfstep_span = sum(pattern[:degree])        # number of halfsteps to each scale degree
-        scale_note_x = (one_index + halfstep_span) % 12  # index of the scale degree in notes
-        pitch_class = notes[scale_note_x]                # the pitch class of the scale degree
+        halfstep_span = sum(pattern[:degree])            # number of halfsteps to each scale degree
+        pitch_class_x = (one_index + halfstep_span) % 12 # index of the pitch class in notes
+        pitch_class = notes[pitch_class_x]               # the pitch class of the scale degree
         scale_letter_x = (one_letter_x + degree) % 7     # index of the letter of the scale degree in letters
         scale_letter = letters[scale_letter_x]           # the letter of the scale degree in letters
 
-        for pc in range(len(pitch_class)):               # check each note of the pitch class
-            if scale_letter == pitch_class[pc][0]:         # do the note letters match?
-                scale.append(pitch_class[pc])                 # append the note to the output list
+        for note in pitch_class:        # check each note of the pitch class
+            if scale_letter == note[0]: # do the note letters match?
+                scale.append(note)      # append the note to the output list
     return scale
