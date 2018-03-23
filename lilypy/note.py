496bd0a3578005cfa8b17faa99a558b73fd70b4d
Michael Gainey <mikeg9b@gmail.com>
2018-03-23 05:40:20 -0500

    before renaming my GitHub repo

--- lilypy/note.py
+++ lilypy/note.py
@@ -1,27 +1,31 @@
-import random
-
 class Note:
-    def __init__(self, pos, pitch_base, pitch_mod, pitch_oct, length):
-        self.pos = pos
-        self.pitch_base = pitch_base
-        self.pitch_mode = pitch_mod
-        self.pitch_oct = pitch_oct
-        self.length = length
+
+    letters = ('a', 'b', 'c', 'd', 'e', 'f', 'g')
+    absolute_pitch = "c'" # the default starting pitch for relative mode
+                          # then, the absolute pitch of the last Note
+
+    def __init__(self, letter, accidental='', change_octave='', duration='8'):
+        self.letter = letter # r = rest
+        self.accidental = accidental
+        self.change_octave = change_octave
+        self.duration = duration # 8 = eighth note
+        self.abs_pitch = ""
+
+        # update the absolute_pitch
+        self.first = Note.letters.index(Note.absolute_pitch[0])
+        self.second = Note.letters.index(self.letter)
+
+        print(Note.letters)
+        print(f'first : {Note.absolute_pitch[0]} {self.first}')
+        print(f"second: {self.letter} {self.second}")
+        diff = (self.second - self.first)
+        if abs(diff) <= 3:
+            print(f"up {diff}")
+        else:
+            print(f"down {7 - diff}")
+
+        Note.absolute_pitch = self.letter
 
 
     def __str__(self):
-        s = ''
-        s += str(self.pos) + ' '
-        s += self.pitch_base
-        return s
-
-notes = []
-for a in range(10):
-    pitch = random.choice(list('abcdefg'))
-    note = Note(a, pitch, '', '', 8)
-    notes.append(note)
-
-for a in range(10):
-    print notes[a]
-
-# change this so that a note's index in the list is the position of the note in the melody
+        return f"{self.letter}{self.accidental} {self.change_octave}"
