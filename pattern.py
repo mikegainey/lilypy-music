6f53e76d71fcea1a192113cb8ddb26fc01c5e1ad
Michael Gainey <mikeg9b@gmail.com>
2018-03-23 19:55:49 -0500

    mostly working version

--- pattern.py
+++ pattern.py
@@ -14,6 +14,47 @@ keys = ['c', 'f', 'bf', ...]
 >>> pattern("c", "1 2 b3 3 5 6 5 3")
 "c d ef e  g a g e"
 """
+from lp_scale import scale
+from modnote import modnote
 
-def pattern(key, pat):
-    pass
+def pattern(key="c", scale_degree=2, pattern="1 b3' r 8  #6 9 b3 5", rhythm="4/4 8 8 8 8 8 8 8 8"):
+    pat1 = scale(key)            # the scale of the key
+    print(f"\npat1:  {pat1}")
+    sdl = pat1[scale_degree - 1] # the chord root
+    pat2 = scale(sdl)            # the (major) scale of the chord
+    print(f"\npat2:  {pat2}")
+    pat3 = []                    #pat2 modified according to pattern
+    for n in pattern.split():
+        note = n # note will mutate
+        if note == "r": # a rest
+            pat3.append("r")
+            continue
+        if note[-1] in "',":
+            octmod = note[-1]
+            note = note[:-1]
+            # print(f"  octmod={octmod}  note={note}")
+        else:
+            octmod = ""
+        if note[0] in "#b":
+            alt = note[0]
+            note = note[1:]
+            # print(f"  alt={alt}  note={note}")
+        else:
+            alt = "na"
+        note = int(note)
+        if note >= 8:
+            note -= 7
+            octmod = "'" # this won't work if there's already an octave modifier
+        letter = pat2[note - 1]
+        letter = modnote(letter, alt)
+        letter = letter + octmod
+
+        pat3.append(letter)
+
+    print()
+    print(pat3)
+
+
+            # pat3.append(note)
+    # print(f"\npat3:  {pat3}")
+
