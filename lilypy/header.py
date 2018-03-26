8191850ea18ab70fd5017c1dafc1674d495df942
Michael Gainey <mikeg9b@gmail.com>
2018-03-26 07:31:42 -0500

    improved header.py to set paper size to "letter"

--- lilypy/header.py
+++ lilypy/header.py
@@ -1,5 +1,6 @@
 header = """\\version "2.18.0"
 \\language "english"
+#(set-default-paper-size "letter")
 \\relative c'{
 \set Staff.extraNatural = ##f"""
 
--- pat1.ly
+++ pat1.ly
@@ -1,5 +1,6 @@
 \version "2.18.0"
 \language "english"
+#(set-default-paper-size "letter")
 \relative c'{
 \set Staff.extraNatural = ##f
 
